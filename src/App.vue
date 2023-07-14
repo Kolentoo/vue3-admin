cff10639ab1bc439f586848a2fe1d9e07f837958
kolento <515835543@qq.com>
2023-07-14 17:28:22 +0800

    项目初始化+ 添加登录页
    Signed-off-by: kolento <515835543@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="kolento">
-    kolento
+    <router-view />
   </div>
 </template>
 
---
+++ src/views/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login">
+    login
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+  .login {
+    background: url('../../public/images/login-bj.jpg') no-repeat center center;
+    background-size: cover;
+    width: 100%;
+    max-width: 100vw;
+    height: 100vh;
+  }
+</style>
